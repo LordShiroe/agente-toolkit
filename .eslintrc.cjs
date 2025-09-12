be4b86895b05c019238d0b6b377803f7c203dbd9
Carlos Angarita <carlomagno127@gmail.com>
2025-09-12 15:55:46 -0500

    fix: format ESLint and lint-staged configuration for consistency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,22 +3,18 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
   rules: {
     // project-specific overrides
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
